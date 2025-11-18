ad4e26f302402e228589f589c32614f8ace19d58
Dicey Devil <amin+git@worksity.com>
2025-11-18 12:28:38 +0000

    Strucutre | Create Worksity Static Html Structre

--- 27_image_carousel/src/index.js
+++ 27_image_carousel/src/index.js
@@ -1,6 +1,3 @@
 import "./styles.css";
 
-console.log("Webpack is working! 🚀");
-
-const container = document.getElementById("carousel-container");
-container.innerHTML = "<p class='text-2xl text-center text-green-600'>Setup Complete! Ready to build the carousel.</p>";
+console.log("Carousel: Ready to add interactivity! 🎠");
