2cbd942870df27bce8556ad31471705df98881c8
Dicey Devil <amin+git@worksity.com>
2025-11-18 09:32:02 +0000

    Setup | Create Minial Source Files

---
+++ 27_image_carousel/src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+console.log("Webpack is working! 🚀");
+
+const container = document.getElementById("carousel-container");
+container.innerHTML = "<p class='text-2xl text-center text-green-600'>Setup Complete! Ready to build the carousel.</p>";
