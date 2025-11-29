64ed66ce159bc14683f89e8e155d141ec03d6c7c
Dicey Devil <amin+git@worksity.com>
2025-11-29 07:39:55 +0000

    Why We Need Async Code

--- 29_weather_app/script.js
+++ 29_weather_app/script.js
@@ -1,3 +1,11 @@
 console.log("First");
-console.log("Second");
+
+console.log("Starting slow operation...");
+// Simulate a slow operation with a loop
+let endTime = Date.now() + 3000; // 3 seconds from now
+while (Date.now() < endTime) {
+    // Do nothing, just wait
+}
+console.log("Slow operation completed!");
+
 console.log("Third");
