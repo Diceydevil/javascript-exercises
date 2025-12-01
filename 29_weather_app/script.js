00269c107db8707ef3db4fe291869ebdb7b79cbe
Dicey Devil <amin+git@worksity.com>
2025-12-01 12:40:06 +0000

    Refactor to Worksity style ui

--- 29_weather_app/script.js
+++ 29_weather_app/script.js
@@ -4,6 +4,8 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${a
 
 console.log("Fetching weather data for", city);
 
+const weatherDisplay = document.getElementById("weather-display");
+
 fetch(url)
     .then(function(response) {
         console.log("Response status:", response.status);
@@ -18,9 +20,24 @@ fetch(url)
         console.log("Weather:", data.weather[0].description);
         console.log("Humidity:", data.main.humidity + "%");
         console.log("Wind speed:", data.wind.speed + " m/s");
+
+        const temp = data.main.temp;
+        const description = data.weather[0].description;
+        const cityName = data.name;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+
+        weatherDisplay.innerHTML = `
+            <h2>${cityName}</h2>
+            <p>Temperature: ${temp}°C</p>
+            <p>Weather: ${description}</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Wind speed: ${windSpeed} m/s</p>
+        `;
     })
     .catch(function(error) {
         console.error("Error:", error);
+        weatherDisplay.innerHTML = `<p>Error: ${error.message}</p>`;
     });
 
 console.log("Request sent!");
