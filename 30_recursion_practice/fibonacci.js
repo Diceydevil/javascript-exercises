2580778704fd81c2116bca473c86941658baa6ce
Dicey Devil <amin+git@worksity.com>
2025-12-05 09:45:32 +0000

    Reccursion for febonacci

--- 30_recursion_practice/fibonacci.js
+++ 30_recursion_practice/fibonacci.js
@@ -1,5 +1,18 @@
-function fibs(n) {
-    return 'hello';
+function fibsRec(n) {
+
+    console.log(`fibsRec(${n}) called`);
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const previousFibs = fibsRec(n - 1);
+    
+    // Calculate the next Fibonacci number
+    const lastOne = previousFibs[previousFibs.length - 1];
+    const lastTwo = previousFibs[previousFibs.length - 2];
+    const nexFibonacciNumber = lastOne + lastTwo;
+
+    return [...previousFibs, nexFibonacciNumber];
 }
 
-console.log(fibs(8));
+console.log('Result:', fibsRec(5)); 
