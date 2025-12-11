0c1d3cea99c3e713e066ce7bd9640debb94d5c87
Dicey Devil <amin+git@worksity.com>
2025-12-11 09:48:38 +0000

    Setting up the contact manager project

---
+++ 31_contact_manager/contact.js
@@ -0,0 +1,9 @@
+const contact1 = {
+  firstName: "John",
+  lastName: "Doe",
+  email: "john.doe@example.com",
+  phone: "123-456-7890",
+  "favorite color": "blue", // Property name with a SPACE
+};
+
+console.log(contact1);
