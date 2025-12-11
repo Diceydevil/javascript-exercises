4d8b83df3b48db8b89afc908c433a373c84f7e34
Dicey Devil <amin+git@worksity.com>
2025-12-11 12:37:42 +0000

    Copy and merge objects with object.assign

--- 31_contact_manager/contact.js
+++ 31_contact_manager/contact.js
@@ -13,9 +13,22 @@ const contact1 = {
   },
 };
 
-let randomVariable = "firstName";
-
 contact1.displayInfo();
 console.log("Keys:", Object.keys(contact1));
 console.log("Values:", Object.values(contact1));
 console.log("Entries:", Object.entries(contact1));
+
+console.log("\n--- Object.assign() ---");
+
+// Create a shallow copy of the contact1 object
+const contact2 = Object.assign({}, contact1);
+contact2.firstName = "Jane";
+
+console.log("Original:", contact1.firstName);
+console.log("Copy:", contact2.firstName);
+
+// Merge objects
+const additionalInfo = { age: 30, city: "New York" };
+const fullContact = Object.assign({}, contact1, additionalInfo);
+
+console.log("Merged Contact:", fullContact);
