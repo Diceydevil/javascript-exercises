b0f1d1f3390e3713a8af90a604cbb3f8989491d5
Dicey Devil <amin+git@worksity.com>
2025-12-11 11:47:43 +0000

    Add methods (functions) to the object

--- 31_contact_manager/contact.js
+++ 31_contact_manager/contact.js
@@ -4,6 +4,13 @@ const contact1 = {
   email: "john.doe@example.com",
   phone: "123-456-7890",
   "favorite color": "blue", // Property name with a SPACE
+
+  displayInfo() {
+    console.log(`Name: ${this.firstName} ${this.lastName}`);
+    console.log(`Email: ${this.email}`);
+    console.log(`Phone: ${this.phone}`);
+    console.log(`Favorite Color: ${this["favorite color"]}`);
+  },
 };
 
-console.log(contact1);
+contact1.displayInfo();
