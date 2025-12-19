e540a45830dc43d16a11fb3ce6be7ba4a138141c
Dicey Devil <amin+git@worksity.com>
2025-12-19 16:13:22 +0000

    Setup a React Environment

---
+++ 32_react_project/my-first-react-app/src/Weather.jsx
@@ -0,0 +1,14 @@
+function Weather() {
+  return (
+    <>
+      <h3>Weather App</h3>
+      <div>
+        <input type="text" placeholder="Enter city name..." value="Bournemouth" />
+        <button>Get Weather</button>
+        <p>Weather data will be displayed here</p>
+      </div>
+    </>
+  );
+}
+
+export default Weather;
--- 32_react_project/my-first-react-app/src/main.jsx
+++ 32_react_project/my-first-react-app/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Greeting from "./Greeting.jsx";
+import Weather from "./Weather.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Greeting />
+    <Weather />
+  </StrictMode>
+);
