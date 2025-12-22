7dc93e62384260aca9ed678ed01e4fd366ffd2c4
Dicey Devil <amin+git@worksity.com>
2025-12-22 05:46:38 +0000

    impliment ProjectCard component, pass data between components using Props, and rendering lists with keys.

--- 33_project_manager/src/App.jsx
+++ 33_project_manager/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
+  const projects = [
+    { id: 1, title: "Project 1", description: "Project description" },
+    { id: 2, title: "Project 2", description: "Project 2 description" },
+    { id: 3, title: "Project 3", description: "Project 3 description" },
+  ];
   return (
     <div className="min-h-screen bg-gray-300">
       <Header />
+      {projects.map((project) => (
+        <ProjectCard
+          key={project.id}
+          title={project.title}
+          description={project.description}
+        />
+      ))}
     </div>
   );
 }
