e1821e28b500222c09adddd0d4beab4e9278dcb2
Dicey Devil <amin+git@worksity.com>
2026-01-16 13:42:39 +0000

    implimentation of SMG: Add api fetch call to get data

--- 33_project_manager/src/components/StudentMemoryGame.jsx
+++ 33_project_manager/src/components/StudentMemoryGame.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+
 function StudentMemoryGame(props) {
+  console.log("StudentMemoryGame component is rendering");
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect hook is running");
+    fetch("https://randomuser.me/api/?results=12")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("API Data:", data);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Student Memory Game - Coming Soon!</h2>
