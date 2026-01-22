be463ecae5b710fef8f5802b166570a9cf663e47
Dicey Devil <amin+git@worksity.com>
2026-01-22 16:19:08 +0000

    Create the Router and use RouterProvider

--- 33_project_manager/src/main.jsx
+++ 33_project_manager/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import "./index.css";
-import App from "./App.jsx";
 import routes from "./routes.jsx";
+import "./index.css";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={createBrowserRouter(routes)} />
+    <RouterProvider router={router} />
   </StrictMode>
 );
